50490fd11bd3909b1c87aab7cb8c149b8bc5284c
hiengianguyen <hiennguyen2060@gmail.com>
2024-12-22 17:35:33 +0700

    uptate

--- public/assets/js/getData.js
+++ public/assets/js/getData.js
@@ -4,25 +4,24 @@ const boxs = document.querySelectorAll(".box");
 let indexOfBoxs = 0;
 function handleData() {
   return new Promise((rs) => {
-    for(const box of boxs) {
+    for (const box of boxs) {
       box.onclick = () => {
         const value = box.getAttribute("data-value");
         const index = box.getAttribute("index");
-        localStorage.setItem('savedValue2', value);
-        localStorage.setItem('indexOfBoxs', index);
+        localStorage.setItem("savedValue2", value);
+        localStorage.setItem("indexOfBoxs", index);
         rs();
-      }
+      };
       indexOfBoxs++;
     }
-
-  })
+  });
 }
 
-await handleData();
-const savedValue2 = localStorage.getItem('savedValue2');
-const indexs2 = localStorage.getItem('indexOfBoxs');
+//await handleData();
+const savedValue2 = localStorage.getItem("savedValue2");
+const indexs2 = localStorage.getItem("indexOfBoxs");
 
 var allSong = songs[indexs2][savedValue2];
 
-// console.log(savedValue2 ," " ,indexs2)
-export {allSong};
+// console.log(savedValue2, " ", indexs2);
+export { allSong };
--- public/assets/js/render.js
+++ public/assets/js/render.js
@@ -1,10 +1,16 @@
-import {listSong} from "./songs.js";
+import { listSong } from "./songs.js";
 
+const body = document.querySelector("body");
+let htmls = `
+<div id="container"></div>
+`;
+body.innerHTML = htmls;
 const conTaiNer = document.querySelector("#container");
-let index1 = 0;
-const boxChildrent = listSong.map((song) => {
-    return`<a ><div class="box" index="${index1++}" data-value="${song}"">${song}</div></a>`
 
+let index1 = 0;
+
+const boxChildrent = listSong.map((song) => {
+  return `<div onclick="(function hi(e) { localStorage.setItem("musicChannel", e.target.dataset.value) })(event)" class="box" index="${index1++}" data-value="${song}">${song}</div>`;
 });
 
 conTaiNer.innerHTML = boxChildrent.join("");
