21386f74fde93ec22d120962271035dfa852200e
hiengianguyen <hiennguyen2060@gmail.com>
2024-12-27 22:46:51 +0700

    add title and icon change follow name song

--- public/assets/js/render.js
+++ public/assets/js/render.js
@@ -4,7 +4,7 @@ const conTaiNer = document.querySelector("#container");
 let index1 = 0;
 
 const boxChildrent1 = songs.map((songChannel) => {
-  return `<div class="box btn-3" index="${index1++}" data-value="${
+  return `<div class="box" index="${index1++}" data-value="${
     songChannel.channel
   }">${songChannel.name}</div>`;
 });
